1e5f69a8941032195c0e19a40a5ba3ef5b657918
Josue HT <jhuarocctapia@gmail.com>
2026-07-08 10:16:59 -0500

    Se agego una url base para la pagina web

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath: "/empadronamiento-difuntos",
   experimental: {
     serverActions: {
       bodySizeLimit: '50mb',
